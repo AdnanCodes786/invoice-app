d213fd48a6dedf4d88243890cc164b79eb9e5f02
cuntAdnan <adnanbarwatiya32@gmail.com>
2025-11-14 22:53:28 +0530

    Added-route

--- backend/src/routes/productRoute.ts
+++ backend/src/routes/productRoute.ts
@@ -1,7 +1,8 @@
 import {Router} from "express";
 import { verifyToken } from "../middlewares/authMiddleware";
+import { createProduct } from "../controllers/productController";
 
 const productsRouter = Router();
 
 
-// productsRouter.post('/add-products',verifyToken,addProducts);
+productsRouter.post('/add-products',verifyToken,createProduct);
